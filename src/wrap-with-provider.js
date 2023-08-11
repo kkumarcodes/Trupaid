3b53688ce9497d67aab07136fe728b58d6270a39
Kyle Kumar <kkumar.codes@gmail.com>
2023-08-11 10:31:31 -0400

    add gatsby module

---
+++ src/wrap-with-provider.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Provider } from 'react-redux';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Auth0Provider from './components/Auth0Provider';
+import { store } from "./store";
+import AppTheme from './assets/theme';
+import { silentAuth } from "./utils/auth";
+import Loader from "./components/Loader";
+
+class SessionCheck extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+
+  handleCheckSession = () => {
+    this.setState({ loading: false });
+  };
+
+  componentDidMount() {
+    silentAuth(this.handleCheckSession);
+  }
+
+  render() {
+    return this.state.loading ? (
+      <Loader />
+    ) : (
+      <React.Fragment>{this.props.children}</React.Fragment>
+    );
+  }
+}
+
+// eslint-disable-next-line react/display-name,react/prop-types
+const WrapProvider = ({ element }) => {
+  // Instantiating store in `wrapRootElement` handler ensures:
+  //  - there is fresh store for each SSR page
+  //  - it will be called only once in browser, when React mounts
+  //   const store = makeStore();
+  return (
+    <CssBaseline>
+      <React.StrictMode>
+        <SessionCheck>
+          <Provider store={store}>
+            <BrowserRouter>
+              <Auth0Provider>
+                <ThemeProvider theme={AppTheme}>
+                  {element}
+                </ThemeProvider>
+              </Auth0Provider>
+            </BrowserRouter>
+          </Provider>
+        </SessionCheck>
+      </React.StrictMode>
+    </CssBaseline>
+  );
+};
+export default WrapProvider;
