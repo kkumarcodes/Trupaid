70222b0df535ba081ad8918700a0c70bf65bc669
Kyle Kumar <kkumar.codes@gmail.com>
2023-08-11 10:31:59 -0400

    Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+function Loader() {
+  return (
+    <div className="dialog-content">
+      <div className="loading-dialog-content">
+        <div className="dialog-loading-content">
+          <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+        </div>
+        <div className="dialog-loading-detail">
+          <p><b>Please wait</b><br />
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
